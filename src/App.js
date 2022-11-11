aaccf7dd157258789247ca6a57f099a72a41e5fe
hjtrahan <harrison_trahan@brown.edu>
2022-11-10 19:13:08 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem"
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,18 +12,30 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const[total, setTotal] = useState(0)
+  const[cart, setCart] = useState([])
+
+  function handleClick(name, price) {
+    setTotal(total + price)
+    setCart([...cart, name])
+  }
+
 
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        <BakeryItem item = {item} handleClick={handleClick}/>
       ))}
 
-      <div>
-        <h2>Cart</h2>
+      <div className="App">
+        <h2>My Cart</h2>
         {/* TODO: render a list of items in the cart */}
+        {cart.map((name) => (
+          <p>{name}</p>
+        ))}
+        <p>Total Price: ${total}</p>
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,23 @@
 // TODO: create a component that displays a single bakery item
+
+
+
+export default function BakeryItem({item, handleClick}) {
+    return (
+        <div className='BakeryItem'>
+            <img src={item.image}></img>
+            <h1>
+                <i>{item.name}</i>
+            </h1>
+            <p>
+                {item.description}
+            </p>
+            <div>
+                <p>
+                    $<i>{item.price}</i>
+                </p>
+                <button onClick={() => handleClick(item.name, item.price)}>Add to Cart</button>
+            </div>
+        </div>
+    )
+}
